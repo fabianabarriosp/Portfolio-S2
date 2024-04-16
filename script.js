68e08d627c463ace42977b2b7e398ce42411e34b
i543558 <f.barriospena@student.fontys.nl>
2024-04-16 14:36:58 +0200

    latest version

--- script.js
+++ script.js
@@ -13,33 +13,30 @@ document.addEventListener('DOMContentLoaded', function() {
   });
   
   
-  //POPUPS - LEARNING OUTCOMES SECTION
-  document.addEventListener('DOMContentLoaded', function() {
+//POPUPS - LEARNING OUTCOMES SECTION
+document.addEventListener('DOMContentLoaded', function() {
     const projectButtons = document.querySelectorAll('.project-button');
   
     // Add event listeners to each project button
     projectButtons.forEach((button, index) => {
       button.addEventListener('click', function() {
-        // Find the closest card element
-        const card = button.closest('.card');
-        if (card) {
-          // Find the corresponding popup container within the card
-          const popupContainer = card.querySelector('.popup-container');
-          if (popupContainer) {
-            popupContainer.style.display = 'block';
+        const popupContainer = document.getElementById(`popup-container-${index + 1}`);
+        if (popupContainer) {
+          popupContainer.style.display = 'block';
   
-            // Add event listener to close button in the popup
-            const closeButton = popupContainer.querySelector('.close-btn');
-            if (closeButton) {
-              closeButton.addEventListener('click', function() {
-                popupContainer.style.display = 'none';
-              });
-            }
+          // Add event listener to close button in the popup
+          const closeButton = document.getElementById(`close-btn-${index + 1}`);
+          if (closeButton) {
+            closeButton.addEventListener('click', function() {
+              popupContainer.style.display = 'none';
+            });
           }
         }
       });
     });
-  });
+});
+
+  
   
   //Figma link for Learning Outcome 1 popup re-branding project
   function openFigma() {
