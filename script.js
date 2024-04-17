16adbe89781efd88697e57fdecf1ba03cdcd148f
i543558 <f.barriospena@student.fontys.nl>
2024-04-17 14:39:51 +0200

    latest version

--- script.js
+++ script.js
@@ -12,31 +12,33 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
-  
 //POPUPS - LEARNING OUTCOMES SECTION
 document.addEventListener('DOMContentLoaded', function() {
     const projectButtons = document.querySelectorAll('.project-button');
-  
+    const popupContainers = document.querySelectorAll('.popup-container');
+
     // Add event listeners to each project button
     projectButtons.forEach((button, index) => {
-      button.addEventListener('click', function() {
-        const popupContainer = document.getElementById(`popup-container-${index + 1}`);
-        if (popupContainer) {
-          popupContainer.style.display = 'block';
-  
-          // Add event listener to close button in the popup
-          const closeButton = document.getElementById(`close-btn-${index + 1}`);
-          if (closeButton) {
-            closeButton.addEventListener('click', function() {
-              popupContainer.style.display = 'none';
+        button.addEventListener('click', function() {
+            // Close all popups before opening the desired one
+            popupContainers.forEach(popupContainer => {
+                popupContainer.style.display = 'none';
             });
-          }
-        }
-      });
+
+            // Open the corresponding popup
+            const popupContainer = document.getElementById(`popup-container-${index + 1}`);
+            popupContainer.style.display = 'block';
+
+            // Add event listener to close button in the popup
+            const closeButton = document.getElementById(`close-btn-${index + 1}`);
+            closeButton.addEventListener('click', function() {
+                popupContainer.style.display = 'none';
+            });
+        });
     });
 });
 
-  
+
   
   //Figma link for Learning Outcome 1 popup re-branding project
   function openFigma() {
@@ -64,3 +66,4 @@ document.addEventListener('DOMContentLoaded', function() {
   }
   
   showSlide(slideIndex);
+
